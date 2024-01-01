620a160141d61a648f1e7079a208254c8f396278
AjaeOnTheRun <syahrul.azrai@gmail.com>
2024-01-01 12:45:50 +0800

    added 'Delete' modal

--- components/NotesList.vue
+++ components/NotesList.vue
@@ -6,13 +6,31 @@
       </div>
       <UDivider class="my-4" />
       <div class="text-sm">
-        Created at: {{ note.id }}
+        Note ID: {{ note.id }}
       </div>
       <template #footer>
         <div class="flex justify-around">
           <UButton label="edit" @click="storeNotes.editNote(note.id)" :to="`/editNote/${note.id}`" variant="link"
             class="text-lg" />
-          <UButton label="delete" @click="storeNotes.deleteNote(note.id)" variant="link" class="text-lg" />
+
+
+          <UButton label="delete" @click="isOpen = true" variant="link" class="text-lg" />
+          <UModal v-model="isOpen" :transition="false">
+            <UCard>
+              <template #header>
+                confirm delete note?
+              </template>
+              <div class="p-4">
+                {{note.note}}
+              </div>
+              <template #footer>
+                <div class="text-right">
+                  <UButton label="Delete" color="rose" variant="outline" @click="storeNotes.deleteNote(note.id)"
+                    class="text-lg" />
+                </div>
+              </template>
+            </UCard>
+          </UModal>
         </div>
       </template>
     </UCard>
@@ -20,5 +38,13 @@
 </template>
 
 <script setup>
+const isOpen = ref(false)
 const storeNotes = useStoreNotes()
+
+/* const props = defineProps({
+  modelValue: {
+    type: String,
+    required: true
+  }
+}) */
 </script>
