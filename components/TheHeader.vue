4d52504ff5e07b8b865d75f15879060a6acdf76c
AjaeOnTheRun <syahrul.azrai@gmail.com>
2024-08-24 14:29:12 +0800

    display notes in console

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,9 +1,17 @@
 <template>
   <div class="text-2xl">
     [ App Header / Settings Menu ]
+    <UButton @click="displayNotes">Display All Notes in Console</UButton>
   </div>
 </template>
 
 <script setup>
+import { useStoreNotes } from '@/stores/storeNotes'
 
+const storeNotes = useStoreNotes()
+
+// Function to display all notes in the console
+const displayNotes = () => {
+  console.log('All Notes:', storeNotes.notes)
+}
 </script>
